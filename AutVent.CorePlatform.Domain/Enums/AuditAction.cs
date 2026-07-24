06740a10a57677f2ac5bd964adb64b882c60c400
John Atoyebi <john@91-payments.com>
2026-07-24 01:58:30 +0100

    updates

--- AutVent.CorePlatform.Domain/Enums/AuditAction.cs
+++ AutVent.CorePlatform.Domain/Enums/AuditAction.cs
@@ -7,6 +7,9 @@ public enum AuditAction
     UserPasswordChanged,
     UserEmailChanged,
 
+    // Inventory
+    StockAdjusted,
+
     // Destructive / Deactivation
     ProductDeleted,
     StaffDeleted,
